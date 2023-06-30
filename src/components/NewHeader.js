94df3aee4201d247e05cfeb28bd52128fb95ce89
Cian Kaiser <studiokaiser1@gmail.com>
2023-06-30 12:05:25 +0300

    responsiveness fixed

--- src/components/NewHeader.js
+++ src/components/NewHeader.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 function NewHeader() {
     return (
         <>
-            <section className="nav">
+            <section className="nav container">
                 <div class="nav-logo">
                     <img src='./assets/logo/MAS (1).png' alt='logo-home' />
                 </div>
@@ -15,7 +15,7 @@ function NewHeader() {
                 <ul className="menu">
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/services'>Services</Link></li>
-                    <li><Link to='/about'>About</Link></li>
+                    <li><Link to='/about-us'>About</Link></li>
                     <li><Link to='/faq'>FAQ</Link></li>
                     <li><Link to='/contact-us'>Contact us</Link></li>
                 </ul>
